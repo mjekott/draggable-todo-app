af111bdc01fa727bde08b437dcb09d6b9b4e12f8
mfonobong ekott <ekottmfon@yahoo.com>
2022-01-02 23:48:40 +0100

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import { useRecoilValue } from "recoil";
 import { GLobalStyle } from "./GlobalStyles";
-import { Router } from "./Router";
-import { isDarkAtom } from "./atoms";
+import { isDarkState } from "./atom";
+
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
-  console.log(isDark);
+  const isDark = useRecoilValue(isDarkState);
+
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
       <GLobalStyle />
-      <Router />
     </ThemeProvider>
   );
 }
